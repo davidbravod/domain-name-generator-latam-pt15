6f297190b8fb219c16b19e63868a7f05b3d30b83
davidbravod <bravod95@gmail.com>
2023-02-14 03:49:26 +0000

    my domain names generator excercise

--- src/app.js
+++ src/app.js
@@ -7,5 +7,31 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+};
+
+let pronouns = ["our", "the", "a", "your"];
+let adjectives = ["great", "big", "little", "awesome"];
+let nouns = ["jogger", "racoon", "dog", "cat"];
+let extensions = [".com", ".org", ".info", ".io"];
+
+let newLi;
+let text;
+let addingLi = document.querySelector("#dominios-lista ol");
+
+window.cambioDominio = function cambioDominio() {
+  for (let i = 0; i < pronouns.length; i++) {
+    for (let j = 0; j < adjectives.length; j++) {
+      for (let k = 0; k < nouns.length; k++) {
+        for (let l = 0; l < extensions.length; l++) {
+          console.log(pronouns[i] + adjectives[j] + nouns[k] + extensions[l]);
+          newLi = document.createElement("li");
+          text = document.createTextNode(
+            pronouns[i] + adjectives[j] + nouns[k] + extensions[l]
+          );
+          newLi.appendChild(text);
+          addingLi.appendChild(newLi);
+        }
+      }
+    }
+  }
 };
